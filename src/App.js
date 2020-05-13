d608c6db95ceef1da1d72961dbee74138d385103
Tran Hai Duong <duongth@rikkeisoft.com>
2020-05-13 17:47:57 +0700

    todo list react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: []
+        }
+    }
+
+    componentWillMount() {
+        if (localStorage && localStorage.getItem('tasks')){
+            var tasks = JSON.parse(localStorage.getItem('tasks'));
+            this.setState({
+                tasks: tasks
+            })
+        }
+    }
+
+    onGenerateData = () => {
+        var tasks = [
+            {
+                id: this.generateID(),
+                name: 'Hoc lap trinh',
+                status: true
+            },
+            {
+                id: this.generateID(),
+                name: 'Di choi',
+                status: true
+            },
+            {
+                id: this.generateID(),
+                name: 'Di an',
+                status: true
+            }
+        ];
+        this.setState({
+            tasks: tasks
+        });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    s4(){
+        return Math.floor((1+Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    generateID(){
+        return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4();
+    }
+
+    render() {
+        var { tasks } = this.state;
+        return (
+            <div className="container">
+                <div className="text-center">
+                    <h1>Quản Lý Công Việc</h1>
+                    <hr />
+                </div>
+                <div className="row">
+                    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <TaskForm />
+                    </div>
+                    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        <button type="button" className="btn btn-primary"><span className="fa fa-plus mr-5"></span>Thêm Công Việc</button>
+                        
+                        <button
+                            type="button"
+                            className="btn btn-danger ml-5"
+                            onClick={this.onGenerateData}
+                        >
+                            Generate Data
+                        </button>
+                        
+                        <Control />
+                        <TaskList tasks={ tasks } />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
